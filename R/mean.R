2a3b94e72c8f3e028f6379aa784742b11d5f6426
Dr. Matthijs Berends <31037261+msberends@users.noreply.github.com>
2022-08-15 10:29:59 +0200

    Create mean.R

---
+++ R/mean.R
@@ -0,0 +1,11 @@
+#' Get the mean of two values
+#' 
+#' This is a test function.
+#' @param x a value
+#' @param y a value
+#' @export
+#' @examples 
+#' mean_test(1, 2)
+mean_test <- function(x, y) {
+  sum(x, y) / 2
+}
